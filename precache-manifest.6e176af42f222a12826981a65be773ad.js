0e873e850e105e60f6ca00ddf17b9110abc9d4d0
tmhuyett <toddhuyett@gmail.com>
2020-09-29 20:32:39 -0400

    Updates

---
+++ precache-manifest.6e176af42f222a12826981a65be773ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34e6c0055599aaa8c95016920e48989a",
+    "url": "/tmhuyett/novgorod.git/index.html"
+  },
+  {
+    "revision": "84a66ca59f87cbd69e0d",
+    "url": "/tmhuyett/novgorod.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9df62a9739152a8fcb18",
+    "url": "/tmhuyett/novgorod.git/static/js/2.65c60152.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tmhuyett/novgorod.git/static/js/2.65c60152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a66ca59f87cbd69e0d",
+    "url": "/tmhuyett/novgorod.git/static/js/main.0c6a915f.chunk.js"
+  },
+  {
+    "revision": "b125a3a4c8e47d0b1a73",
+    "url": "/tmhuyett/novgorod.git/static/js/runtime-main.f2d54574.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tmhuyett/novgorod.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
